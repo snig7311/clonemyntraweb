4aee86d3fb690b410670e274ab4a90a7e1e280b5
snig7311 <132433111+snig7311@users.noreply.github.com>
2023-10-05 22:32:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function Redirect() {
+    location.href="https://www.myntra.com";
+ }
+ function Skincare() {
+    location.href="https://www.myntra.com/skin-care?rawQuery=skin%20care";
+ }
+ function Watch() {
+    location.href="https://www.myntra.com/watches";
+ }
+ function Headphone() {
+    location.href="https://www.myntra.com/headphone?rawQuery=headphone";
+ }
+ function Camera() {
+    location.href="https://www.myntra.com/camera?rawQuery=camera";
+ }
+ function Nike() {
+    location.href="https://www.myntra.com/nike-shows?rawQuery=nike%20shows";
+ }
+ function Beauty() {
+    location.href="https://www.myntra.com/beauty-product?rawQuery=beauty%20product";
+ }
